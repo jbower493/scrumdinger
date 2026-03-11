79a470094c1d5acf9235ca7f1e0a4fcd784745c9
Jamie Bower <j_bower@hotmail.co.uk>
2026-03-11 21:19:45 +1100

    feat: display shared text in main app

--- scrumdinger/ContentView.swift
+++ scrumdinger/ContentView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    init() {
-        print("mate") // works here
+    @State private var sharedText: String = "Nothing shared yet"
+    
+    func readSharedData() {
+        let defaults = UserDefaults(suiteName: "group.com.scrumdingerJbower")
+
+        if let type = defaults?.string(forKey: "shared_type"),
+           let value = defaults?.string(forKey: "shared_value") {
+            sharedText = "\(type): \(value)"
+        }
     }
     
     var body: some View {
@@ -18,6 +25,11 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text(sharedText)
+            .padding()
+            .onAppear {
+                readSharedData()
+            }
         }
         .padding()
     }
